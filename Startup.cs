56fca658e10d5d7e2e76f6617de87f4a3bdad287
Timmy <github@trashcompactor.co.uk>
2018-04-22 20:01:42 +0100

    Testing an update

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace AzureTest3
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World, this is an update!");
             });
         }
     }
