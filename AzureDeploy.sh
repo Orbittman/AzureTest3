681121c772259905db943dbe3238b7d7fd8b803f
Timmy <github@trashcompactor.co.uk>
2018-04-22 07:59:25 +0100

    Added web.config

---
+++ AzureDeploy.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/bash
+
+gitrepo=https://github.com/Orbittman/AzureTest3
+token=5c37acc36af346e723ab942bc1c17b028622f824
+webappname=mywebapp$RANDOM
+
+# Create a resource group.
+az group create --location westeurope --name myResourceGroup
+
+# Create an App Service plan in `FREE` tier.
+az appservice plan create --name $webappname --resource-group myResourceGroup --sku S1 --is-linux
+
+# Create a web app.
+az webapp create --name $webappname --resource-group myResourceGroup --plan $webappname --runtime "dotnetcore|2.0"
+
+# Configure continuous deployment from GitHub. 
+# --git-token parameter is required only once per Azure account (Azure remembers token).
+az webapp deployment source config --name $webappname --resource-group myResourceGroup \
+--repo-url $gitrepo --branch master --git-token $token
+
+# Copy the result of the following command into a browser to see the web app.
+echo http://$webappname.azurewebsites.net
